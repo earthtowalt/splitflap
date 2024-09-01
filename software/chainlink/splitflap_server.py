8556212f9be31987b1dd01f0558ae3f6908e6f37
Jack Walton <jackwalton00@aol.com>
2024-09-01 15:20:22 -0500

    more easter eggs

--- software/chainlink/splitflap_server.py
+++ software/chainlink/splitflap_server.py
@@ -1,13 +1,25 @@
 import sys
 from pathlib import Path
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from splitflap_proto import splitflap_context, ask_for_serial_port
 from splitflap_proto import Splitflap
 
 # from API import get_quote
 
+import random
 import time
+
+
+NO_FLAPPY = False
+
+punishment_links = ['https://www.youtube.com/watch?v=IrF2Fut0bXw', 'https://www.istockphoto.com/photo/middle-finger-gm140472118-3415406']
+
+easter_eggs = {
+    'jack': 'yeahhhhh... 😎'
+}
+
+bad_actors = dict()
 
 app = Flask(__name__)
 
@@ -28,19 +40,40 @@ input_form = '''
 def index():
     user_input = None
     mean = False
+    warn_about_behavior = False
+    output = ''
+
+    if bad_actors.get(request.host, -1) >= 3:
+        bad_actors[request.host] = 0
+        warn_about_behavior = True
 
     if request.method == 'POST': 
         if request.form.get('submit_button') == 'Tell me a Joke':
-            print('no')
-            send_splitflap_text('no')
+            output = 'no'
         else:
-            user_input = request.form.get('user_input')
-            if user_input in bad_words:
+            user_input = request.form.get('user_input').lower()
+            if any(x in user_input for x in bad_words):
                 mean = True
-            print(user_input)
-            send_splitflap_text(user_input)
+                bad_actors[request.host] = bad_actors.get(request.host, 0) + 1
+                print(bad_actors, request.host)
+                if bad_actors[request.host] >= 3:
+                    return redirect(random.choice(punishment_links), code=302)
+            output = user_input
     
-    return render_template('home.html', user_input=user_input, mean=mean)
+        if not NO_FLAPPY:
+            print(output)
+            send_splitflap_text(output)
+
+    if user_input:
+        default_response = f'You last entered: "{user_input}".'
+        if mean:
+            default_response += " (and that wasn't very nice!)"
+        response_text = easter_eggs.get(user_input, default_response)
+    
+    else:
+        response_text = None
+
+    return render_template('home.html', response_text=response_text, mean=mean, warn_about_behavior=warn_about_behavior)
 
 # First endpoint - Root URL "/"
 @app.route('/update', methods=['GET', 'POST'])
@@ -101,6 +134,10 @@ def send_splitflap_text(text):
 
 
 if __name__ == '__main__':
+
+    if NO_FLAPPY:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+        sys.exit(0)
 
     if len(sys.argv) > 1:
         port = sys.argv[1]
