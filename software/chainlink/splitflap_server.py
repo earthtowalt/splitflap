05c5da2ee477c31455f35b96b19b920e8c1e4204
Jack Walton <jackwalton00@aol.com>
2024-09-01 13:50:02 -0500

    make nicer home page

--- software/chainlink/splitflap_server.py
+++ software/chainlink/splitflap_server.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 from splitflap_proto import splitflap_context, ask_for_serial_port
 from splitflap_proto import Splitflap
 
@@ -10,18 +10,40 @@ import time
 
 app = Flask(__name__)
 
-input_form = '''
-        <form method="post">
-            <label for="user_input">Enter text:</label>
-            <input type="text" id="user_input" name="user_input">
-            <input type="submit" value="Submit">
-        </form>
-    '''
+with open('badwords.txt', 'r') as f:
+    bad_words = [x.strip() for x in f.readlines()]
 
+input_form = '''
+<form method="post">
+    <label for="user_input">Enter text:</label>
+    <input type="text" id="user_input" name="user_input">
+    <input type="submit" value="Submit">
+</form>'''
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+@app.route('/home/', methods=['GET', 'POST'])
+def index():
+    user_input = None
+    mean = False
+
+    if request.method == 'POST': 
+        if request.form.get('submit_button') == 'Tell me a Joke':
+            print('no')
+            # send_splitflap_text('no')
+        else:
+            user_input = request.form.get('user_input')
+            if user_input in bad_words:
+                mean = True
+            print(user_input)
+            # send_splitflap_text(user_input)
+    
+    return render_template('home.html', user_input=user_input, mean=mean)
 
 # First endpoint - Root URL "/"
-@app.route('/update/', methods=['GET', 'POST'])
-def index():
+@app.route('/update', methods=['GET', 'POST'])
+def update():
     if request.method == 'POST':
         user_input = request.form.get('user_input')
         send_splitflap_text(user_input)
@@ -65,9 +87,8 @@ def magic2():
     return ""
 
 def send_splitflap_text(text): 
-    # Show a random word every 10 seconds
     sanitized = text[:6].lower()
-
+    
     print('setting text: ' + sanitized)
     s.set_text(sanitized, force_movement=Splitflap.ForceMovement.ONLY_NON_BLANK)
 
@@ -79,11 +100,15 @@ def send_splitflap_text(text):
 
 
 if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    sys.exit(0)
+
     if len(sys.argv) > 1:
         port = sys.argv[1]
     else:
         port = ask_for_serial_port()
         
     with splitflap_context(port) as s:
-        app.run(host='192.168.1.68', port=5000)
+        app.run(host='0.0.0.0', port=5000)
 
